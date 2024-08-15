801bb36833c0ce8dd74e09ca5475f22c4e9df49a
ozodbec <151632769+ozodbec@users.noreply.github.com>
2024-08-15 21:01:12 +0500

    Add files via upload

---
+++ src/components/ProductsList.jsx
@@ -0,0 +1,25 @@
+import { useLoaderData, Link } from "react-router-dom";
+
+function ProductsList() {
+  const { products } = useLoaderData();
+  console.log(products);
+  return (
+    <ul className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+      {products.map((product) => {
+        return (
+          <Link to={`/product/${product.id}`}>
+            <li className="card shadow mb-3" key={product.id}>
+              <img className="rounded-xl h-60" src={product.category.image} alt={product.title} />
+              <div className="card-body flex flex-col">
+                <h2 className="font-bold text-xl">{product.title.slice(0, 19)}</h2>
+                <p className="font-semibold relative bottom-0">{product.price}$</p>
+              </div>
+            </li>
+          </Link>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default ProductsList;
